edf6fede4452ab69b487cad4b3335d35c52c69e9
Burconst <36280804+Burconst@users.noreply.github.com>
2019-04-30 18:32:11 +0300

    Update CreateDropTables.sql

--- CreateDropTables.sql
+++ CreateDropTables.sql
@@ -1,4 +1,4 @@
-﻿-- CREATE DATABASE Regional_geography_db;
+-- CREATE DATABASE Regional_geography_db;
 -- GO
 -- USE Regional_geography_db;
 
@@ -15,11 +15,25 @@ CREATE TABLE Country (
 	Continent_ID INT NOT NULL,
 	Country_ID INT IDENTITY NOT NULL,
 	Name VARCHAR(100) NOT NULL,
-	State_Languages VARCHAR(50) NOT NULL,
 	Total_Area INT NOT NULL,
 	Population_Size INT NOT NULL,
 	CONSTRAINT Country_PK PRIMARY KEY (Country_ID),
 	FOREIGN KEY (Continent_ID)  REFERENCES Continent (Continent_ID)
+)
+
+CREATE TABLE CountryLanguage(
+	ID INT NOT NULL,
+	Country_ID INT NOT NULL,
+	Language_ID INT NOT NULL,
+	CONSTRAINT CountryLanguage_PK PRIMARY KEY (ID),
+	FOREIGN KEY (Country_ID) REFERENCES Country (Country_ID),
+	FOREIGN KEY (Language_ID) REFERENCES Language (Language_ID)
+)
+
+CREATE TABLE Language(
+	Language_ID INT NOT NULL,
+	Name VARCHAR(50) NOT NULL,
+	CONSTRAINT Language_FK PRIMARY KEY (Language_ID)
 )
 
 CREATE TABLE City (
@@ -31,47 +45,25 @@ CREATE TABLE City (
 	FOREIGN KEY (Country_ID)  REFERENCES Country (Country_ID)
 )
 
-CREATE TABLE City_sight (
-	City_Sight_ID INT IDENTITY NOT NULL,
+CREATE TABLE Sight (
+	Sight_ID INT IDENTITY NOT NULL,
 	Name VARCHAR(100) NOT NULL,
 	City_ID INT NOT NULL,
 	OpeningHours VARCHAR(100) NOT NULL,
-	CitySightType_ID INT NOT NULL,
-	Address VARCHAR(200)
+	SightType_ID INT NOT NULL,
+	Address VARCHAR(200) NOT NULL,
+	Description VARCHAR(200),
 	CONSTRAINT City_Sight_PK PRIMARY KEY (City_Sight_ID),
 	FOREIGN KEY (City_ID)  REFERENCES City (City_ID),
-	FOREIGN KEY (CitySightType_ID)  REFERENCES City_Sight_Type (CitySightType_ID)
+	FOREIGN KEY (SightType_ID)  REFERENCES Sight_Type (SightType_ID)
 )
 
-CREATE TABLE Natural_Sight (
-	Nearest_City_ID INT NOT NULL,
-	Name VARCHAR(100) NOT NULL,
-	SightType_ID INT NOT NULL,
-	IsUNESCO BIT NOT NULL,
-	Main_Characteristic_ID INT,
-	FOREIGN KEY (Nearest_City_ID)  REFERENCES City (Nearest_City_ID),
-	FOREIGN KEY (SightType_ID)  REFERENCES Natural_Sight_Type (SightType_ID),
-	FOREIGN KEY (Main_Characteristic_ID)  REFERENCES UniqueCharacteristic (Characteristic_ID)
-)
-
-CREATE TABLE Natural_Sight_Type (
+CREATE TABLE Sight_Type (
 	SightType_ID INT IDENTITY NOT NULL,
 	Name VARCHAR(100) NOT NULL,
 	CONSTRAINT SightType_PK PRIMARY KEY (SightType_ID)
 )
 
-CREATE TABLE City_Sight_Type (
-	CitySightType_ID INT IDENTITY NOT NULL,
-	Name VARCHAR(100) NOT NULL,
-	CONSTRAINT CitySightType_PK PRIMARY KEY (CitySightType_ID),
-)
-
-CREATE TABLE UniqueCharacteristic (
-	Characteristic_ID INT IDENTITY NOT NULL,
-	Name VARCHAR(50) NOT NULL,
-	Value INT NOT NULL,
-	CONSTRAINT Characteristic_PK PRIMARY KEY (Characteristic_ID)
-)
 --
 
 
@@ -84,9 +76,8 @@ CREATE TABLE UniqueCharacteristic (
 DROP TABLE Continent;
 DROP TABLE Country;
 DROP TABLE City;
-DROP TABLE City_sight;
-DROP TABLE Natural_Sight;
+DROP TABLE Sight;
 DROP TABLE Sight_Type;
-DROP TABLE City_Sight_Type;
-DROP TABLE UniqueCharacteristic;
+DROP TABLE CountryLanguage;
+DROP TABLE Language;
 --
